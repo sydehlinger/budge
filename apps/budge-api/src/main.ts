d418d36a75e39cc2e82b624d1942b95d2955f699
Sydney Ehlinger <sydney.ehlinger@gmail.com>
2025-04-29 23:59:41 -0500

    adding in frontend to docker

--- apps/budge-api/src/main.ts
+++ apps/budge-api/src/main.ts
@@ -2,7 +2,7 @@ import Fastify from 'fastify';
 import { app } from './app/app';
 
 const host = process.env.HOST ?? '0.0.0.0';
-const port = process.env.PORT ? Number(process.env.PORT) : 3000;
+const port = process.env.PORT ? Number(process.env.PORT) : 8080;
 
 // Instantiate Fastify with some config
 const server = Fastify({
--- apps/budge/vite.config.ts
+++ apps/budge/vite.config.ts
@@ -7,7 +7,7 @@ export default defineConfig(() => ({
   cacheDir: '../../node_modules/.vite/apps/budge',
   server: {
     port: 4200,
-    host: 'localhost',
+    host: '0.0.0.0',
   },
   preview: {
     port: 4300,
